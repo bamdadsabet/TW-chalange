46aae49cc642bb89258be5168505308eabe202b6
bamdadsabet <bamdadsabet@gmail.com>
2025-02-22 08:21:42 +0330

    feat: Implimented Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { IButtonProps, ButtonTypeEnum } from "./types"
+function Button({type = ButtonTypeEnum.PRIMARY, icon, children, action}: IButtonProps) {
+  return (
+    <button 
+      onClick={action} 
+      className={`button ${type === ButtonTypeEnum.PRIMARY ? 'button--primary' : 'button--outlined'}`}
+    >
+      {children}
+      {
+        !!icon && (
+          <img src={icon} alt="icon" />
+        )
+      }
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Button/index.ts
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import { ButtonTypeEnum, IButtonProps } from "./types";
+
+export { Button }
+
+export type { IButtonProps, ButtonTypeEnum }
