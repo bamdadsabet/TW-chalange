5575d18f733691df1bf46938c13cc434a20ccbd0
bamdadsabet <bamdadsabet@gamil.com>
2025-02-23 15:57:47 +0330

    feat: Added hero

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { Hero } from "./components/Hero"
 import { Navbar } from "./components/Navbar"
 import { navbar } from "./MockData"
 import { hero } from "./MockData"
+import { episodes } from './MockData'
+import { EpisodesList } from "./components/Episodes"
 function App() {
   return (
     <section className="bg-black h-screen">
       <Navbar links={navbar} />
       <Hero {...hero} />
+      {/* <EpisodesList {...episodes} /> */}
       <Footer />
     </section>
   )
