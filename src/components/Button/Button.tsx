8814ed6aef5f8c436bebf3f5428f4f1bf41545f2
bamdadsabet <bamdadsabet@gmail.com>
2025-02-26 14:56:25 +0330

    style: Button mobile view

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,15 @@
 import { IButtonProps } from "./types"
 import './Button.css'
-function Button({type = 'primary', icon, children, action}: IButtonProps) {
+function Button({type = 'primary', icon, children, action, className = ''}: IButtonProps) {
   return (
     <button 
       onClick={action} 
-      className={`button ${type === 'primary' ? 'button--primary' : 'button--outlined'}`}
+      className={`button ${type === 'primary' ? 'button--primary' : 'button--outlined'} ${!children ? 'button--icon' : ''} ${className}`}
     >
       {children}
       {
         !!icon && (
-          <img src={icon} alt="icon" />
+          <img className="button__icon" src={icon} alt="icon" />
         )
       }
     </button>
